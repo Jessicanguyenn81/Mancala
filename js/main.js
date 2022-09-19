0ceae8d9f77e418e7f17d2cdd0f8a7ac516f83a9
Jessicanguyenn81 <Jessicanguyenn81@gmail.com>
2022-09-19 10:41:11 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+ /*----- constants -----*/
+const PLAYER_LOOKUP = {
+    '1': {
+        name: 'Player 1'
+    },
+    '-1': {
+        name: 'Player 2'
+    }
+}
+
+  /*----- state variables -----*/
+let turn, board, winner
+
+  /*----- cached elements  -----*/
+
+
+  /*----- event listeners -----*/
+
+
+  /*----- functions -----*/
