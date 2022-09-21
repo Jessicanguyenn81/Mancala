4b4bafc46e42ddb1207ae5ac7061037e82b41daa
Jessicanguyenn81 <Jessicanguyenn81@gmail.com>
2022-09-21 12:15:39 -0700

    render function working

--- js/main.js
+++ js/main.js
@@ -18,12 +18,12 @@ const boardEl = document.getElementById('board')
 
   /*----- functions -----*/
   init()
-  
+
   function init() {
     turn = 1;
     board = [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0];
     winner = null
-    // render()
+    render()
 }
 
 function handleResetClick(){
@@ -34,10 +34,12 @@ function handleBoardClick(evt) {
 //this shows which pod was clicked 
     console.log(evt.target.id)
     let bowlId = evt.target.id
+    let afterTurn = lastMarblePosition(bowlId, board[bowlId])
     if(winner) return; 
     let currentIndex = bowlId
     let currentCount = board[bowlId]
     for (let i= 0; i < board[bowlId]; i++) {
+        console.log(i)
         if (turn === 1) {
             //if player 1 is on index 13 they skip it and go to index 5
             if (currentIndex === 13) {
@@ -63,8 +65,18 @@ function handleBoardClick(evt) {
             currentIndex++
         }
     }
-
+    board[bowlId] = 0
+    if (!afterTurn) {
+        turn *= -1
+    }
+    render()
 }
+
+
+
+
+
+
 
 // function scoreBoard() {
 //     document.querySelector(scoreboard1) = `Player 1: ${board[6]}`;
@@ -74,6 +86,13 @@ function handleBoardClick(evt) {
 
 
 
+function lastMarblePosition(position, value){
+    while (value !== 0) {
+        value--
+        position++
+    }
+    return position === 6 || position === 13
+}
 
 
 
@@ -85,16 +104,11 @@ function handleBoardClick(evt) {
 
 
 
-
-// function render() {
-//     messageDisplayEl.innerText = `${PLAYER_LOOKUP[1]}'s Turn `
-//     // pseudocode update board state to show current value of each pod on board
-//     //pseudocode let marbles = the value inside each pod
-//     board.forEach((bowl, idx) => {
-//         if(bowl){
-            
-//         } else {
-
-//         }
-//     })
-// }}
+function render() {
+    messageDisplayEl.innerText = `${PLAYER_LOOKUP[1]}'s Turn `
+    // pseudocode update board state to show current value of each pod on board
+    //pseudocode let marbles = the value inside each pod
+    board.forEach((bowl, idx) => {
+        document.getElementById(idx).innerText = bowl
+    })
+}
