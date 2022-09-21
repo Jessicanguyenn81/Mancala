ea30266543bafd7d47b609089f41ab30f7223836
Jessicanguyenn81 <Jessicanguyenn81@gmail.com>
2022-09-21 15:56:57 -0700

    rewrote handleClick

--- js/main.js
+++ js/main.js
@@ -30,77 +30,31 @@ function handleResetClick(){
     console.log('Reset Button clicked')
 }
 
+
 function handleBoardClick(evt) {
-//this shows which pod was clicked 
-    console.log(evt.target.id)
-    let bowlId = evt.target.id
-    let afterTurn = lastMarblePosition(bowlId, board[bowlId])
-    if(winner) return; 
-    let currentIndex = bowlId
-    let currentCount = board[bowlId]
-    for (let i= 0; i < board[bowlId]; i++) {
-        console.log(i)
-        if (turn === 1) {
-            //if player 1 is on index 13 they skip it and go to index 5
-            if (currentIndex === 13) {
-                currentIndex = 5
-                currentCount--
-            }
-        }
-        if (turn === -1) {
-            //if player -1 is on index 6 they skip it and go to index 7
-            if (currentIndex === 6) {
-                currentIndex = 7
-                currentCount--
-            }
-        }
-        //this will subtract the marbles while increasing the board position
-        if (currentIndex === 13) {
-            board[currentIndex]++
-            currentIndex = 0
-            currentCount--
+    const returnIds = ['scoreboard1', '13', 'player-one', 'player-two', 'scoreboard2', '6']
+    const startingIdx = evt.target.id
+    if (returnIds.includes(startingIdx) || !board[startingIdx]) return 
+    let numOfMoves = board[startingIdx]
+    let currentIdx = Number(startingIdx) + 1
+    board[startingIdx] = 0
+    for (let i = 0; i < numOfMoves; i++) {
+        const skip = (turn === 1 && currentIdx === 13) || (turn === -1 && currentIdx === 6)
+        if (skip) {
+            i -= 1
         } else {
-            board[currentIndex]++
-            currentCount--
-            currentIndex++
+            board[currentIdx] += 1
         }
-    }
-    board[bowlId] = 0
-    if (!afterTurn) {
-        turn *= -1
-    }
+        if (currentIdx + 1 === board.length){
+                currentIdx = 0
+        } else {
+                currentIdx++ 
+            }
+    } 
+    turn *= -1
     render()
+   
 }
-
-
-
-
-
-
-
-// function scoreBoard() {
-//     document.querySelector(scoreboard1) = `Player 1: ${board[6]}`;
-//     document.querySelector(scoreboard2)= `Player 2: ${board[13]}`;
-// }
-
-
-
-
-function lastMarblePosition(position, value){
-    while (value !== 0) {
-        value--
-        position++
-    }
-    return position === 6 || position === 13
-}
-
-
-
-
-
-
-
-
 
 
 
